c6fcf37e81b5d9a830779db74d2b7a88bfb7a6f4
Alwan Mortada <almortada88@gmail.com>
2015-06-24 08:12:55 -0700

    Update newDrinkAJAX.js

--- app/assets/javascripts/newDrinkAJAX.js
+++ app/assets/javascripts/newDrinkAJAX.js
@@ -1,8 +1,16 @@
+// In the forked repo, go find some code that you did NOT work on.
+// Add some comments that describe what that piece of code is doing. 
+// Do this for the whole file.
+
+// Jquery waitting for the document be ready to run
 $(function(){
-
+	
+	// event listner waitting for user to click submit
 	$("#new_drink").submit(function(event){
+		// preventing the default auto resoponse inhareted 
 		event.preventDefault();
-
+		
+		// the next three lines are getting the values fromt he input boxes and outting them in var's 
 		var drink_name = $("#drink_name").val();
 
 		var directions = $("#drink_directions").val();
@@ -13,13 +21,17 @@ $(function(){
 
 	});
 
-
+	// declaring a function drinkCreation and passing arguments 
 	function drinkCreation(drink_name, directions, picture){
-
+		// using jquery to call an ajax method
 		$.ajax({
+			// type of request post to page
 			type: "post",
+			// post to /drinks page
 			url: "/drinks",
+			// data type jason
 			dataType: "JSON",
+			// the content of the data posted
 			data: {
 				drink: {
 					name: drink_name,
@@ -27,13 +39,16 @@ $(function(){
 					picture: picture
 				}
 			},
-
+			
+			// declaring an anonymous function
 			success: function(data, textStatus, jqXHR){
-
+				// return this data in such form 
 				var html = "<li>"+drink_name+"</li>"+"<li>"+directions+"</li>"+"<li>"+picture+"</li>";
+				// prepend the var html to the result area
 				$("#results").prepend(html);
 
 			},
+			// if any error happends display an error depending on the type of the error
 			error: function(jqXHR, textStatus, errorThrown){
 				alert("ERROR");
 			}
